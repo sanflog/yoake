3d60abb6fe26d5d0aa141337eff05652beab470b
sanflog <daichan613@gmail.com>
2023-01-22 14:50:42 +0900

    test

--- yoake/settings.py
+++ yoake/settings.py
@@ -121,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Static Root setting
+STATIC_ROOT = 'static/'
